fd8e367fb3df49051e5da915917b75bba3251a49
Kamana Tripathi <kamanatripathi01@gmail.com>
2025-07-22 21:02:06 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Http\Request;
 
-if (!defined('LARAVEL_START')) {
-    define('LARAVEL_START', microtime(true));
-}
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
